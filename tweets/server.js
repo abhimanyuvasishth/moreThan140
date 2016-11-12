caa3bbedc8a398930e715db9745999065bb94aed
abhimanyu <abhimanyu.vasishth@gmail.com>
2016-11-12 19:53:05 +0400

    tweet output written to file

--- tweets/server.js
+++ tweets/server.js
@@ -1,8 +1,22 @@
-var Twit = require('twit');
+var Twit    = require('twit');
 var request = require("request");
-var twit_collection = [];
+var fs      = require('fs');
 
+var Data = function(){
+  this.user_name;
+  this.tweet;
+  this.country;
+  this.detailed_location;
+  this.date;
+}
 
+var objects = [];
+var d = new Date();
+var day = d.getDate();
+var month = d.getMonth()+1;
+var year = d.getFullYear();
+var date = day+ '' + month+ '' +year;
+var nice_date = day+ '/' + month+ '/' +year;
 
 var options = {
   consumer_key : 'kij47kAvvt6tPIVit99ATR3kq',
@@ -13,54 +27,47 @@ var options = {
 }
 
 var bot = new Twit(options);
-bot.get("search/tweets", {q:'emissions', count: 10}, function(error,data, response){
-    //console.log(data.statuses);
-    
-    for(var result in data.statuses){
-        console.log(result);
-        var text = data.statuses[result].text;
-        var user_name = data.statuses[result].user.name;
+bot.get("search/tweets", {q:'emissions', count: 10}, function(error,data,response){  
+  
+  for(var result in data.statuses){
+    var text = data.statuses[result].text;
+    var user_name = data.statuses[result].user.name;
 
-        var location = data.statuses[result].user.location;
-        if (location){
-          createData(location, text, user_name)
-        }
-
-
+    var location = data.statuses[result].user.location;
+    if (location){
+      createData(location, text, user_name)
     }
-
+  }
 });
 
-function createData(_location, _text, _user_name){
+function createData(location, text, user_name){
   
-  var url = "https://maps.google.com/maps/api/geocode/json?address=" + _location + "&sensor=false";
-  //console.log(url);
+  var url = "https://maps.google.com/maps/api/geocode/json?address=" + location + "&sensor=false";
 
   request({
       url: url,
       json: true
-  }, function (error, response, body) {
-
-      if (!error && response.statusCode === 200) {
-
-          //console.log(body.results[0]); // Print the json response
-          console.log("-------------------");
-          if(body.status === "OK" ){
-            var i = 0;
-            for(i; i<body.results[0].address_components.length; i++){
-              if (body.results[0].address_components[i].types[0]==="country"){
-                var country = body.results[0].address_components[i].short_name;
-                var twit_data = {user_name: _user_name, twit: _text, location: country, loc: _location};
-                twit_collection.push(twit_data);
-                console.log(_user_name);
-                console.log(_text);
-                console.log(country);
-              
-              }
-            }
+  }, 
+  function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      if(body.status === "OK" ){
+        for(var i=0; i<body.results[0].address_components.length; i++){
+          if (body.results[0].address_components[i].types[0]==="country"){
+            var country = body.results[0].address_components[i].short_name;
+            var data = new Data();
+            data.user_name = user_name;
+            data.tweet = text;
+            data.country = country;
+            data.detailed_location = location;
+            data.date = nice_date;
+            objects.push(data);
+            fs.writeFile('output/' + date+'.json', JSON.stringify(objects, null, 4), function(err){
+              if (err) console.log(err);
+              else console.log('File successfully written! - Check your project directory for the file');
+            })
           }
+        }
       }
+    }
   })
-             
 }
-
